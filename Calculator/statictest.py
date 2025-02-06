50ff8024d6751ad98e29822b72fe8f196ce1de1b
Nidhun <hello@example.com>
2025-02-05 20:44:21 -0500

    added static unit test

---
+++ Calculator/statictest.py
@@ -0,0 +1,15 @@
+import unittest
+from calculator import Calculator
+
+class TestCalculator(unittest.TestCase):
+
+    def test_add_2_and_3(self):
+        calculator = Calculator()
+        self.assertAlmostEqual(calculator.add(2, 3), 5)
+
+    def test_add_minus_1_and_1(self):
+        calculator = Calculator()
+        self.assertAlmostEqual(calculator.add(-1, 1), 0)
+
+if __name__ == '__main__':
+    unittest.main()
